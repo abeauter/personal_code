fffc8aa2d2e145db527ff87b683b54335e9b0de3
Avery Beauter <beauteravery@hotmail.com>
2024-07-20 23:10:27 -0400

    Hope I am fixing what I done goofed.

--- ruby/idle_survivor/CharacterSkills.rb
+++ ruby/idle_survivor/CharacterSkills.rb
@@ -1,27 +1,30 @@
-class Character
-  
-  character_skills = {
-    'agility' => 0.0,
-	'attack' => 0.0,
-	'botany' => 0.0,
-	'cartography' => 0.0,
-	'cooking' => 0.0,
-	'crafting' => 0.0,
-	'defense' => 0.0,
-	'farming' => 0.0,
-	'firemaking' => 0.0,
-	'fishing' => 0.0,
-	'fletching' => 0.0,
-	'gathering' => 0.0,
-	'harvesting' => 0.0,
-	'hit_points' => 0.0,
-	'hunting' => 0.0,
-	'mining' => 0.0,
-	'ranged' => 0.0,
-	'smithing' => 0.0,
-	'sneak' => 0.0,
-	'strength' => 0.0,
-	'thieving' => 0.0,
-	'township' => 0.0,
-	'woodcutting' => 0.0
-}
+class Character  
+  def initializor 
+  	@character_skills = {
+  	  'agility' => 0.0,
+			'attack' => 0.0,
+			'botany' => 0.0,
+			'cartography' => 0.0,
+			'cooking' => 0.0,
+			'crafting' => 0.0,
+			'defense' => 0.0,
+			'farming' => 0.0,
+			'firemaking' => 0.0,
+			'fishing' => 0.0,
+			'fletching' => 0.0,
+			'gathering' => 0.0,
+			'harvesting' => 0.0,
+			'hit_points' => 0.0,
+			'hunting' => 0.0,
+			'mining' => 0.0,
+			'ranged' => 0.0,
+			'smithing' => 0.0,
+			'sneak' => 0.0,
+			'strength' => 0.0,
+			'thieving' => 0.0,
+			'township' => 0.0,
+			'woodcutting' => 0.0
+		}
+	end	
+end 
+	
